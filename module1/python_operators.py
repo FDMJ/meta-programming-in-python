68cba7db96e8b8e9edd3bd755839fafd008c85cd
FDMJ <martins_julian@gmx.de>
2024-06-10 17:28:11 +0200

    operators

---
+++ module1/python_operators.py
@@ -0,0 +1,17 @@
+print(5 + 3)
+print(5 - 3)
+print(5 * 3)
+print(5 / 3)
+print(5 % 3)
+
+a = True
+b = False
+
+if a and b:
+    print("All true!")
+
+if a or b:
+    print("All true!")
+
+if not(a) or not(b):
+    print("All false but not makes them true!")
